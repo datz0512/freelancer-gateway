0b18a940237a001b20d67e2079df3f3d035b820d
datz <datblu2003@gmail.com>
2024-04-11 23:51:19 +0700

    add users controllers and routes

---
+++ src/routes/buyer.ts
@@ -0,0 +1,20 @@
+import express, { Router } from 'express';
+
+import { Get } from '@gateway/controllers/users/buyer/get';
+
+class BuyerRoutes {
+  private router: Router;
+
+  constructor() {
+    this.router = express.Router();
+  }
+
+  public routes(): Router {
+    this.router.get('/buyer/email', Get.prototype.email);
+    this.router.get('/buyer/username', Get.prototype.currentUsername);
+    this.router.get('/buyer/:username', Get.prototype.username);
+    return this.router;
+  }
+}
+
+export const buyerRoutes: BuyerRoutes = new BuyerRoutes();
